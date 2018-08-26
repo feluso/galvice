ef880b0bc407415709f38f7a9e229aabf4afdefc
GAL <feluso2@gmail.com>
2018-08-25 21:59:39 -0500

    Centered the galvice, worked out an issue with the size of the sprite, tried a new component but having trouble on how i want to position it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './menu/info/info.component';
 
 const routes : Routes = [
-  { path: 'info', component: MenuComponent }, 
+  { path: 'info', component: InfoComponent, outlet: 'popup' }, 
+  { path: 'menu', component: MenuComponent }, 
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,

c56f2e7495de9df7d6e476966569b54a8ccae6fd
feluso <feluso2@gmail.com>
2018-11-08 00:10:12 -0500

    fixed a bug with measureable skills

--- src/model/skill.model.ts
+++ src/model/skill.model.ts
@@ -7,7 +7,7 @@ export class Skill {
     constructor(name?: String, group?: boolean, level?: Number) {
         this.name = name;
         this.group = group;
-        this.measureable = level ? true : false;
+        this.measureable = level > 0 ? true : false;
         this.level = level;
     }
 

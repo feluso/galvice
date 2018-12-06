1107b24a919e1e1533443f1d4ae1c5a1f12f4187
feluso <feluso2@gmail.com>
2018-12-06 02:07:54 -0500

    changed the grid of the menu a little, added a little observable for messages in the future

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-device',
@@ -13,10 +14,23 @@ export class DeviceComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const asyncMessages = Observable.create(function (observer) {
+      const messages = ['Welcome!', 'More info on the left', 'Feed me on the right'];
 
-    setInterval(() => {
-      this.actualMessage = 'Welcome!!!!';
-    }, 5000);
+      for (let index = 0; index < messages.length; index++) {
+        const message = messages[index];
+        setTimeout(() => {
+          observer.next(message);
+        }, index * 1000);
+
+      }
+    });
+
+    asyncMessages.subscribe(
+      message => {
+        this.actualMessage = message;
+      }
+    );
   }
 
 }

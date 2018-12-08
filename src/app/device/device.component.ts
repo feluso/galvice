1ef34edacc74bfe66e60c8df4a228e3851477829
feluso <feluso2@gmail.com>
2018-12-08 00:52:59 -0500

    fixed some cursos and added very hacky animations for the character!

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+enum state {IDLE, PET, EATING}
+
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
@@ -10,18 +12,20 @@ export class DeviceComponent implements OnInit {
 
   title = 'app';
   actualMessage: String = '';
+  stateImage: String = 'assets/images/at.gif';
+  public state: state = state.IDLE;
 
   constructor() { }
 
   ngOnInit(): void {
     const asyncMessages = Observable.create(function (observer) {
-      const messages = ['Welcome!', 'More info on the left', 'Feed me on the right'];
+      const messages = ['Welcome!', 'More info on the left', 'Feed me on the right', ''];
 
       for (let index = 0; index < messages.length; index++) {
         const message = messages[index];
         setTimeout(() => {
           observer.next(message);
-        }, index * 1000);
+        }, index * 3000);
 
       }
     });
@@ -32,5 +36,18 @@ export class DeviceComponent implements OnInit {
       }
     );
   }
+
+
+  pet(): void {
+    this.state = state.PET;
+    this.stateImage = 'assets/images/happy.gif';
+    setTimeout(() => {
+      this.stateImage = 'assets/images/at.gif';
+      this.state = state.IDLE;
+    }, 6000);
+  }
+
+
+
 
 }
